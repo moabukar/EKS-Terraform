0a78f1e2209e5f3af826962cc680950015cbdc05
mohamedA007 <mo-abukar10@hotmail.com>
2021-10-22 15:01:00 +0100

    VPC and EKS cluster added

---
+++ vpc.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = "eu-west-2"
+  description = "AWS London region"
+}
+
+# variable "vpc_cidr_block" {}
+# variable "private_subnet_cidr_blocks" {}
+# variable "public_subnet_cidr_blocks" {}
+
+data "aws_availability_zones" "azs" {}
+
+module "TEST-vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.66.0"
+
+  name            = "TEST-vpc"
+  cidr            = var.vpc_cidr_block
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
+  azs             = data.aws_availability_zones.azs.names
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"                  = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"         = 1
+  }
+
+}
